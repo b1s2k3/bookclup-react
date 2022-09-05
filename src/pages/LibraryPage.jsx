8d8559e7cb89361012f1abc46fed345324e85c33
b1s2k3 <91268003+b1s2k3@users.noreply.github.com>
2022-09-05 21:49:17 +0300

    Update LibraryPage.jsx

--- src/pages/LibraryPage.jsx
+++ src/pages/LibraryPage.jsx
@@ -31,38 +31,4 @@ const LibraryPage = () => {
 export default LibraryPage;
 
 
-// import React, { useContext } from "react";
-// import { useState } from "react";
-// import LibraryItem from "../components/LibraryItems";
-// import LibraryContex from "../context/Context";
 
-// const LibraryPage = () => {
-//   const { libItems, setLibItem } = useContext(LibraryContex);
-//   const [searchLib, setSearchLib] = useState("");
-
-//   const searchLibBook = () => {
-//     const search = libItems.filter(
-//       (libItem) => libItem.title.toLowerCase() === searchLib.toLowerCase()
-//     );
-//     setLibItem(search);
-//   };
-//   return (
-//     <div className="library-page">
-//       <div className="lib-search">
-//       <input
-//               placeholder="Enter book name.."
-//               value={searchLib}
-//               onChange={(e) => setSearchLib(e.target.value)}
-//             />
-//             <button onClick={() => searchLibBook(searchLib)}>search</button>
-//       </div>
-//       <div>
-//       {libItems.map(libItem => (
-//         <LibraryItem libItem={libItem} searchLibBook={searchLibBook}/>
-//       ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default LibraryPage;
