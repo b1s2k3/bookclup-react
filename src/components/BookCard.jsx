deaefa36b55c95b66cda321213614d262be41585
b1s2k3 <91268003+b1s2k3@users.noreply.github.com>
2022-09-06 08:44:19 +0300

    Update BookCard.jsx

--- src/components/BookCard.jsx
+++ src/components/BookCard.jsx
@@ -18,20 +18,4 @@ const BookCard = ({ book: {id, title, author, year }}) => {
   );
 };
 
-
-// Without props destruction
-
-// const BookCard = (props) => {
-//   return (
-//     <div className="book-item">
-//       <h4>
-//         <b>{props.book.title}</b>
-//       </h4>
-//       <h5>Written by {props.book.author}</h5>
-//       <h6>{props.book.year}</h6>
-//       <button className="add-book">+</button>
-//     </div>
-//   );
-// };
-
 export default BookCard;
